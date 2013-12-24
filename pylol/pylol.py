2580e888ed564083a84f5ad1624a4ad8795a7786
Charles Lai <cjl223@cornell.edu>
2013-12-23 19:05:41 -0600

    add MIT liscense
    will later add MIT liscenses to all of my public projects

--- pylol/pylol.py
+++ pylol/pylol.py
@@ -1,6 +1,27 @@
-#pylol.py
-#Charles J. Lai
-#December 21, 2013
+
+"""
+The MIT License (MIT)
+
+Copyright (c) 2013 Charles J. Lai
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 
 import urllib2
 import json
@@ -14,15 +35,6 @@ class Pylol(object):
     official Riot Games League of Legends developer API. More info in the 
     README file. Anything not clarified in module documentation can be
     found gleaned by consulting the Riot API documentation.
-
-    Fields
-    ------
-    api_key: User-defined key from Riot Games
-    region: User-defined region for API call parameters
-
-    Methods
-    -------
-
     """
     #Constants:
     URL_BASE = 'http://prod.api.pvp.net/api/lol/'
@@ -120,7 +132,7 @@ class Pylol(object):
 
     def get_league(self,id):
         """
-        Returns:
+        Returns: 
 
         Precondtions: id is an integer number
         """
